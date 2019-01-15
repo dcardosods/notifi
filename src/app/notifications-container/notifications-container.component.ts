f0013e73c7143fb2796d4f56809c44dbf62bcd79
Daniel Cardoso <danielcardosods@gmail.com>
2019-01-15 15:26:14 -0200

    Move notification grouping to the service

--- src/app/notifications-container/notifications-container.component.ts
+++ src/app/notifications-container/notifications-container.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, from } from 'rxjs';
-import * as _ from 'lodash';
 
 import { NotificationsService } from '../notifications.service';
+import { NotificationItem } from '../notification-item';
 
 @Component({
   selector: 'app-notifications-container',
@@ -10,7 +10,7 @@ import { NotificationsService } from '../notifications.service';
   styleUrls: ['./notifications-container.component.scss']
 })
 export class NotificationsContainerComponent implements OnInit {
-  notifications: any[] = [];
+  notifications: NotificationItem[] = [];
 
   constructor(private notificationsService: NotificationsService) { }
 
@@ -20,22 +20,7 @@ export class NotificationsContainerComponent implements OnInit {
   toggleNotifications() {
     this.notificationsService.getAll()
       .subscribe(notifications => {
-        this.notifications = this.groupNotifications(notifications);
-        console.log(this.notifications);
+        this.notifications = notifications;
       });
-  }
-
-  private groupNotifications(notifications) {
-    const orderedByDay = _.orderBy(notifications, ['updatedOn'], ['desc']);
-    const groupedByDay = _.groupBy(orderedByDay, item => item.updatedOn.replace(/^(\d{4}-\d{2}-\d{2})(.+)$/, '$1'));
-    const groupedByEvent = _.map(groupedByDay, group => _.groupBy(group, 'event'));
-    const normalized = _.map(groupedByEvent, group => {
-      const event = Object.keys(group)[0];
-      return {
-        event,
-        items: group[event]
-      };
-    });
-    return normalized;
   }
 }
--- src/app/notifications.service.ts
+++ src/app/notifications.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import * as _ from 'lodash';
 
 import { environment } from '../environments/environment';
+import { NotificationItem } from './notification-item';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +14,22 @@ export class NotificationsService {
 
   constructor(private http: HttpClient) { }
 
-  getAll(): Observable<any> {
-    return this.http.get(`${environment.apiUrl}/CodeChallenge`);
+  getAll(): Observable<NotificationItem[]> {
+    return this.http.get(`${environment.apiUrl}/CodeChallenge`)
+      .pipe(map(this.groupNotifications));
+  }
+
+  private groupNotifications(notifications): NotificationItem[] {
+    const orderedByDay = _.orderBy(notifications, ['updatedOn'], ['desc']);
+    const groupedByDay = _.groupBy(orderedByDay, item => item.updatedOn.replace(/^(\d{4}-\d{2}-\d{2})(.+)$/, '$1'));
+    const groupedByEvent = _.map(groupedByDay, group => _.groupBy(group, 'event'));
+    const normalized = _.map(groupedByEvent, group => {
+      const event = Object.keys(group)[0];
+      return {
+        event,
+        items: group[event]
+      };
+    });
+    return normalized;
   }
 }
