26ce5445350c1838f13c83973e42f81394195fb9
Daniel Cardoso <danielcardosods@gmail.com>
2019-01-15 14:18:12 -0200

    Create components and fetch data from server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { NotificationsContainerComponent } from './notifications-container/notifications-container.component';
+import { NotificationsItemComponent } from './notifications-item/notifications-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotificationsContainerComponent,
+    NotificationsItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/notifications-container/notifications-container.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable, from } from 'rxjs';
+import * as _ from 'lodash';
+
+import { NotificationsService } from '../notifications.service';
+
+@Component({
+  selector: 'app-notifications-container',
+  templateUrl: './notifications-container.component.html',
+  styleUrls: ['./notifications-container.component.scss']
+})
+export class NotificationsContainerComponent implements OnInit {
+  notifications: any[] = [];
+
+  constructor(private notificationsService: NotificationsService) { }
+
+  ngOnInit() {
+  }
+
+  toggleNotifications() {
+    this.notificationsService.getAll()
+      .subscribe(notifications => {
+        this.notifications = this.groupNotifications(notifications);
+        console.log(this.notifications);
+      });
+  }
+
+  private groupNotifications(notifications) {
+    const orderedByDay = _.orderBy(notifications, ['updatedOn'], ['desc']);
+    const groupedByDay = _.groupBy(orderedByDay, item => item.updatedOn.replace(/^(\d{4}-\d{2}-\d{2})(.+)$/, '$1'));
+    const groupedByEvent = _.map(groupedByDay, item => _.groupBy(item, 'event'));
+    return groupedByEvent;
+  }
+}
